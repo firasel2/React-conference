ce9233ca5417b7a41a97b3eb0cfe60a7a1ebf721
firasel2 <rasel@lemonhive.com>
2023-08-04 22:54:43 +0600

    Update tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-5xl text-center font-semibold text-emerald-300">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h2 className="text-4xl font-semibold text-center text-primary">
         React Conference
       </h2>
     </main>
