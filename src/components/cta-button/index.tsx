0209398da972fda0d895fd9e2e8bf35cc667d57f
firasel2 <rasel@lemonhive.com>
2023-08-05 01:31:44 +0600

    Implement cta-button

--- src/components/cta-button/index.tsx
+++ src/components/cta-button/index.tsx
@@ -1,8 +1,31 @@
+import { twMerge } from 'tailwind-merge';
 import CustomLink from '../costom-link';
 import { ICtaButton } from './interface';
+import IconStore from '../icons';
 
-const CtaButton = (props: ICtaButton) => {
-  return <CustomLink {...props} />;
+const CtaButton = ({
+  href,
+  isOpenNewTab,
+  className,
+  prefetch,
+  label,
+  icon,
+}: ICtaButton) => {
+  return (
+    <CustomLink
+      href={href}
+      isOpenNewTab={isOpenNewTab}
+      prefetch={prefetch}
+      className={twMerge(
+        'w-fit min-h-[3.75rem] py-4 px-6 rounded-[1.875rem] bg-primary flex items-center justify-center overflow-hidden border-0 ',
+        className
+      )}
+    >
+      <span className="flex items-center justify-center flex-wrap gap-2 text-lg font-medium text-black">
+        {label} {icon && <IconStore iconName={icon} />}
+      </span>
+    </CustomLink>
+  );
 };
 
 export default CtaButton;
