483eeb3bba39fbe5236390f1f6ae7bc0ed193d59
firasel2 <rasel@lemonhive.com>
2023-08-25 20:19:41 +0600

    feat: update sidebar builder data with dnd

--- src/builder/sections/sidebar/index.tsx
+++ src/builder/sections/sidebar/index.tsx
@@ -1,44 +1,46 @@
+'use client';
+
 import Sidebar from '@src/sections/sidebar';
-import { useParams, usePathname } from 'next/navigation';
-import { ReactNode } from 'react';
+import { ISidebar } from '@src/sections/sidebar/interface';
+import { useParams } from 'next/navigation';
+import { ReactNode, useState } from 'react';
 
 const SidebarSectionBuilder = ({ children }: { children?: ReactNode }) => {
   const { id } = useParams();
-  const pathName = usePathname();
+
+  const [sidebarItems, setSidebarItems] = useState<ISidebar['sidebarItems']>([
+    {
+      id: '01',
+      href: `/${id}/organizer`,
+      title: 'Organizer',
+      activeKey: '/organizer',
+      icon: 'Top Bottom Arrow',
+    },
+    {
+      id: '02',
+      href: `/${id}/speakers`,
+      title: 'Speakers',
+      activeKey: '/speakers',
+      icon: 'Top Bottom Arrow',
+    },
+    {
+      id: '03',
+      href: `/${id}/schedule`,
+      title: 'Schedule',
+      activeKey: '/schedule',
+      icon: 'Top Bottom Arrow',
+    },
+    {
+      id: '04',
+      href: `/${id}/sponsors`,
+      title: 'Sponsors',
+      activeKey: '/sponsors',
+      icon: 'Top Bottom Arrow',
+    },
+  ]);
 
   return (
-    <Sidebar
-      sidebarItems={[
-        {
-          id: '01',
-          href: `/${id}/organizer`,
-          title: 'Organizer',
-          active: pathName.includes('/organizer'),
-          icon: 'Top Bottom Arrow',
-        },
-        {
-          id: '02',
-          href: `/${id}/speakers`,
-          title: 'Speakers',
-          active: pathName.includes('/speakers'),
-          icon: 'Top Bottom Arrow',
-        },
-        {
-          id: '03',
-          href: `/${id}/schedule`,
-          title: 'Schedule',
-          active: pathName.includes('/schedule'),
-          icon: 'Top Bottom Arrow',
-        },
-        {
-          id: '04',
-          href: `/${id}/sponsors`,
-          title: 'Sponsors',
-          active: pathName.includes('/sponsors'),
-          icon: 'Top Bottom Arrow',
-        },
-      ]}
-    >
+    <Sidebar sidebarItems={sidebarItems} setSidebarItems={setSidebarItems}>
       {children}
     </Sidebar>
   );
