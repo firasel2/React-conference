1d5c5959d8f1070f1c358c75389b6c7efcda77e2
firasel2 <rasel@lemonhive.com>
2023-08-25 18:10:15 +0600

    feat: update interface, mock-data and section builder data

--- src/builder/sections/sidebar/index.tsx
+++ src/builder/sections/sidebar/index.tsx
@@ -10,24 +10,28 @@ const SidebarSectionBuilder = ({ children }: { children?: ReactNode }) => {
     <Sidebar
       sidebarItems={[
         {
+          id: '01',
           href: `/${id}/organizer`,
           title: 'Organizer',
           active: pathName.includes('/organizer'),
           icon: 'Top Bottom Arrow',
         },
         {
+          id: '02',
           href: `/${id}/speakers`,
           title: 'Speakers',
           active: pathName.includes('/speakers'),
           icon: 'Top Bottom Arrow',
         },
         {
+          id: '03',
           href: `/${id}/schedule`,
           title: 'Schedule',
           active: pathName.includes('/schedule'),
           icon: 'Top Bottom Arrow',
         },
         {
+          id: '04',
           href: `/${id}/sponsors`,
           title: 'Sponsors',
           active: pathName.includes('/sponsors'),
--- src/components/sidebar-item/interface.ts
+++ src/components/sidebar-item/interface.ts
@@ -1,6 +1,7 @@
 import { IconNames } from '@components/icons/interface';
 
 export interface ISidebarItem {
+  id: string;
   title: string;
   href: string;
   active?: boolean;
